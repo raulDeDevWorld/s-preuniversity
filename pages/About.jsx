0f567d1a2b273c3f8e8d2b307cf20236317f87f7
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-08-23 12:30:48 -0400

    DC

--- pages/About.jsx
+++ pages/About.jsx
@@ -6,12 +6,12 @@ import style from '../styles/About.module.css'
 
 function About() {
     return (
-        <PageLayout>   
+        <PageLayout>     
             <div className={style.container}>
             <img src="logo-hazlo-simple.svg" className={style.logo} alt="logo" />
                 <h3 className={style.subtitle}>Acerca de:</h3>
                 <p className={style.paragraph}>
-                    Swoou Preuniversity es un una plataforma de educación mantenida por Swoou Company, que intenta coadyubar en el proceso de enseñanza y aprendizaje de matemáticas a niños y niñas que se estan inciando en la etapa escolar. <br />
+                    Swoou Preuniversity es un una plataforma de educación mantenida por Swoou Company, que intenta coadyubar en el proceso de enseñanza y aprendizaje de  a niños y niñas que se estan inciando en la etapa escolar. <br />
                     Deseas saber mas acerca de nuestras <br />
                     <Link href="/Policy">
                         <a className={style.policy}>Politicas de Servicio?</a>
